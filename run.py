08a6644993ed7aa28e2b5c233b3720f5151dc306
mikakallberg <mikaela.kallberg@hotmail.com>
2022-05-05 17:24:38 +0000

    Add function for user to start game

--- run.py
+++ run.py
@@ -6,12 +6,22 @@ tprint('mikaela')
 
 randomword = ['furthermore', 'copying', 'periodic', 'mental']
 
+start_game = input('Do you wish to start? Press y for yes or n for no: ')
+if start_game == ('y'):
+    print('game starting')
+elif start_game == ('n'):
+    print('break statement')
+
+start_game()
+
+
 def pick_random_word(randomword):
     """
     This function selects a random word from source
     """
     word_in_play = random.choice(randomword)
     print(word_in_play)
+
 
 pick_random_word(randomword)
 
