2cfc7eb5cce8908fd7cac07a2d8787a1e09f6753
mikakallberg <mikaela.kallberg@hotmail.com>
2022-05-06 19:33:22 +0000

    Add vaildation to user input data, bug in function

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # Use limited array first to get code working
 import random
-from art import * 
+from art import *
 
 tprint('mikaela')
 
@@ -9,7 +9,7 @@ whateverword = ['furthermore', 'copying', 'periodic', 'mental']
 letters_guessed = ""
 
 # This variabel lets user initiate the game
-# Credit for code structure (not a complete copy paste) to 
+# Credit for code structure (not a complete copy paste) to
 # Code Grepper, please see README for link.
 print('This is a hangman type game.')
 print('The computer will generate a random word.')
@@ -17,10 +17,31 @@ print('The number of times you have to guess the letters of that word,')
 print('depends on the number of letters in that random word.')
 print('You get 2 extra attempts with each word.')
 start_game = input('Do you wish to start? Press y for yes or n for no: ')
+"""
 if start_game == ('y'):
     print('Lets build a bear!')
 elif start_game == ('n'):
     print('Okey, have a nice day!')
+"""
+
+def validate_input_start(input_correct):
+    """
+    Checkes to see if input from user is y or n
+    """
+    user_input = start_game
+    try: 
+        if user_input == ('y'):
+            print('Lets build a bear!')
+        elif user_input == ('n'):
+            print('Okey, have a nice day!')
+        if user_input != ('y') or ('n'):
+            raise ValueError(
+                f'You inserted an incorrect value, please try again'
+            )
+            return False
+        return True
+
+
 
 
 def pick_random_word(randomword):
@@ -40,7 +61,6 @@ def calculate_max_turns(word):
     print(word)
     max_turns = (len(word)+2)
     print(f'You have {max_turns} guesses for this word!')
-    
     return max_turns
 
 
@@ -59,9 +79,8 @@ def loop_letters(lives_left, word_in_play):
             print(f'{guess} was incorrect. You have {lives_left} attempts left.')
             break
 
-
+validation = validate_input_start(x)
 generated_word = pick_random_word(whateverword)
 print(generated_word)
 number_of_lives_left = calculate_max_turns(generated_word)
 loop_letters(number_of_lives_left, generated_word)
-
