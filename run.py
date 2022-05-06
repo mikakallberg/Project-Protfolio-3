107f151b416f1620907098829d149b5877c3126b
mikakallberg <mikaela.kallberg@hotmail.com>
2022-05-06 08:08:25 +0000

    Add number of guesses based on letters in randomword

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ tprint('mikaela')
 
 randomword = ['furthermore', 'copying', 'periodic', 'mental']
 
+#This variabel lets user initiate the game
 start_game = input('Do you wish to start? Press y for yes or n for no: ')
 if start_game == ('y'):
     print('game starting')
@@ -16,10 +17,13 @@ elif start_game == ('n'):
 def pick_random_word(randomword):
     """
     This function selects a random word from source
+    and generates the number of guesses a user has
     """
     if start_game == 'y':
         word_in_play = random.choice(randomword)
+        max_turns = len(word_in_play)
         print(word_in_play)
+        print(max_turns)
 
 
 pick_random_word(randomword)
